86526de5ad67bc7d7543850d6dd575e914946e01
guimoura0202 <guiavilamou@gmail.com>
2025-10-23 20:03:41 -0300

    fix: ajustando o eslint.config.js de acordo com o prettie

--- minha-loja/eslint.config.js
+++ minha-loja/eslint.config.js
@@ -1,10 +1,15 @@
-import js from "@eslint/js";
-import globals from "globals";
-import pluginReact from "eslint-plugin-react";
-import { defineConfig } from "eslint/config";
+import js from '@eslint/js';
+import globals from 'globals';
+import pluginReact from 'eslint-plugin-react';
+import { defineConfig } from 'eslint/config';
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs,jsx}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser } },
+  {
+    files: ['**/*.{js,mjs,cjs,jsx}'],
+    plugins: { js },
+    extends: ['js/recommended'],
+    languageOptions: { globals: globals.browser },
+  },
   pluginReact.configs.flat.recommended,
   {
     rules: {
@@ -13,7 +18,7 @@ export default defineConfig([
     },
     settings: {
       react: {
-        version: 'detect', 
+        version: 'detect',
       },
     },
   },
